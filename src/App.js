e8ac9733cabdc7145e8c9d6c1f2d6d5cfb5f9615
naim0018 <mdkazinaim0018@gmail.com>
2025-10-02 09:08:36 +0600

    removed js file

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-// src/App.tsx
-import { Outlet } from "react-router-dom";
-function App() {
-    return (_jsx("div", { className: "app", children: _jsx(Outlet, {}) }));
-}
-export default App;
--- src/components/layout/Wrapper.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-const Wrapper = ({ children }) => {
-    return (_jsx("div", { className: "max-w-[1920px] mx-auto lg:px-40", children: children }));
-};
-export default Wrapper;
