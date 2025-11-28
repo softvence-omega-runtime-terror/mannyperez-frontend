b59461d1f40d8d0949e2a7b9f3aca941d359da5a
zxayed <zzayedghost@gmail.com>
2025-11-28 17:33:45 +0600

    improved ui of order page

--- src/pages/OrderDetails.tsx
+++ src/pages/OrderDetails.tsx
@@ -18,28 +18,7 @@ import React, { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { toast } from "sonner";
 
-// const STATUS_OPTIONS: OrderStatus[] = [
-//   "pending",
-//   "processing",
-//   "shipped",
-//   "delivered",
-//   "cancelled",
-// ];
 
-// const badgeClass = (status?: string) => {
-//   switch ((status || "").toLowerCase()) {
-//     case "processing":
-//       return "bg-blue-100 text-blue-700 border-blue-200";
-//     case "shipped":
-//       return "bg-indigo-100 text-indigo-700 border-indigo-200";
-//     case "delivered":
-//       return "bg-green-100 text-green-700 border-green-200";
-//     case "cancelled":
-//       return "bg-red-100 text-red-700 border-red-200";
-//     default:
-//       return "bg-yellow-100 text-yellow-700 border-yellow-200";
-//   }
-// };
 
 const formatDate = (value?: string) => {
   if (!value) return "N/A";
@@ -54,7 +33,7 @@ const OrderDetails: React.FC = () => {
   const { orderId } = useParams<{ orderId: string }>();
   const navigate = useNavigate();
  const currentUser = useAppSelector(selectCurrentUser);
- console.log("🚀 ~ OrderDetails ~ currentUser:", currentUser)
+
 
 
   const { data, isLoading, refetch } = useGetSingleOrderQuery(orderId ?? "", {
